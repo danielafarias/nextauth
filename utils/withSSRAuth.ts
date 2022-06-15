b8c7b843df68f0a59c9ee84a5983986093d095b9
Daniela Farias <farias.danicris@gmail.com>
2022-06-15 11:45:43 -0300

    Validate page permission

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,3 +1,4 @@
+import decode from "jwt-decode";
 import {
   GetServerSideProps,
   GetServerSidePropsContext,
@@ -5,14 +6,24 @@ import {
 } from "next";
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../errors/AuthTokenError";
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: WithSSRAuthOptions
+) {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies["nextauth.token"];
 
-    if (!cookies["nextauth.token"]) {
+    if (!token) {
       return {
         redirect: {
           destination: "/",
@@ -21,22 +32,40 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
       };
     }
 
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if(!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: "/dashboard",
+            permanent: false,
+          }
+        }
+      }
+    }
+
     try {
       return await fn(ctx);
     } catch (err) {
       if (err instanceof AuthTokenError) {
         destroyCookie(ctx, "nextauth.token");
         destroyCookie(ctx, "nextauth.refreshToken");
-  
+
         return {
           redirect: {
             destination: "/",
             permanent: false,
-          }
-        }
+          },
+        };
       }
-
-      throw err;
     }
   };
 }
